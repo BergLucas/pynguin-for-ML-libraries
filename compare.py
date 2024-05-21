315baee3b91af17eb3b7bf8a93fd2da698da8ff9
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-05-21 10:46:00 +0200

    Add Mann–Whitney U-test in compare.py

--- compare.py
+++ compare.py
@@ -1,18 +1,39 @@
+from scipy.stats import mannwhitneyu
 import argparse
 import json
+import csv
+import os
+
+
+def get_coverages(experiment_path: str, nb_runs: int) -> list[float]:
+    coverages = []
+    for i in range(nb_runs):
+        run_path = os.path.join(experiment_path, str(i))
+
+        statistics_path = os.path.join(run_path, "statistics.csv")
+
+        with open(statistics_path, "r") as f:
+            (statistics,) = csv.DictReader(f)
+            coverage = float(statistics["Coverage"])
+            coverages.append(coverage)
+
+    return coverages
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("first_summary")
-    parser.add_argument("second_summary")
+    parser.add_argument("first_experiment")
+    parser.add_argument("second_experiment")
 
     args = parser.parse_args()
 
-    with open(args.first_summary, "r") as f:
+    first_summary_path = os.path.join(args.first_experiment, "summary.json")
+    second_summary_path = os.path.join(args.second_experiment, "summary.json")
+
+    with open(first_summary_path, "r") as f:
         first_summary = json.load(f)
 
-    with open(args.second_summary, "r") as f:
+    with open(second_summary_path, "r") as f:
         second_summary = json.load(f)
 
     keys = [
@@ -30,6 +51,19 @@ def main() -> None:
         first_summary_value = str(first_summary[key])
         second_summary_value = str(second_summary[key])
         print(f"{key:<25}: {first_summary_value:<35} {second_summary_value:<35}")
+
+    nb_runs = first_summary["nb_runs"]
+
+    assert nb_runs == second_summary["nb_runs"]
+
+    first_coverages = get_coverages(args.first_experiment, first_summary["nb_runs"])
+    second_coverages = get_coverages(args.second_experiment, second_summary["nb_runs"])
+
+    t_statistic, p_value = mannwhitneyu(
+        first_coverages, second_coverages, alternative="two-sided"
+    )
+
+    print(f"Mann–Whitney U-test      : {t_statistic} (pvalue: {p_value})")
 
 
 if __name__ == "__main__":
