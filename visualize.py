031e02603dde7efd7c914699cda48a4997db5e02
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-05-22 02:27:57 +0200

    Add support to visualize one or more than 2 experiments

--- visualize.py
+++ visualize.py
@@ -1,52 +1,69 @@
 import matplotlib.pyplot as plt
+from functools import reduce
+import operator
 import argparse
 import json
 import os
 
 
+def load_experiment(experiment_path: str) -> tuple[str, int, dict]:
+    summary_path = os.path.join(experiment_path, "summary.json")
+
+    with open(summary_path, "r") as f:
+        summary = json.load(f)
+
+    return (
+        summary["experiment_name"],
+        summary["nb_runs"],
+        summary["executed_lines_counter"],
+    )
+
+
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("first_experiment")
-    parser.add_argument("second_experiment")
+    parser.add_argument("experiments", nargs="+")
 
     args = parser.parse_args()
 
-    first_summary_path = os.path.join(args.first_experiment, "summary.json")
-    second_summary_path = os.path.join(args.second_experiment, "summary.json")
+    all_experiment_name, all_nb_runs, all_executed_lines_counter = zip(
+        *(load_experiment(experiment_path) for experiment_path in args.experiments)
+    )
 
-    with open(first_summary_path, "r") as f:
-        first_summary = json.load(f)
-
-    with open(second_summary_path, "r") as f:
-        second_summary = json.load(f)
-
-    first_experiment_name = first_summary["experiment_name"]
-    second_experiment_name = second_summary["experiment_name"]
-    total_nb_runs = first_summary["nb_runs"] + second_summary["nb_runs"]
-
-    first_executed_lines_counter: dict = first_summary["executed_lines_counter"]
-    second_executed_lines_counter: dict = second_summary["executed_lines_counter"]
+    total_nb_runs = sum(all_nb_runs)
 
     lines = sorted(
-        first_executed_lines_counter.keys() | second_executed_lines_counter.keys(),
+        reduce(
+            operator.or_,
+            (
+                executed_lines_counter.keys()
+                for executed_lines_counter in all_executed_lines_counter
+            ),
+        ),
         key=lambda line: int(line),
     )
 
-    first_count = [first_executed_lines_counter[line] for line in lines]
-    second_count = [second_executed_lines_counter[line] for line in lines]
-
     fig, ax = plt.subplots()
 
-    ax.bar(lines, first_count, label=first_experiment_name)
-    ax.bar(lines, second_count, label=second_experiment_name, bottom=first_count)
+    bottom = [0] * len(lines)
+    for experiment_name, executed_lines_counter in zip(
+        all_experiment_name, all_executed_lines_counter
+    ):
+        sorted_executed_lines_counter = [executed_lines_counter[line] for line in lines]
+        ax.bar(
+            lines,
+            sorted_executed_lines_counter,
+            label=experiment_name,
+            bottom=bottom,
+        )
+        bottom = [sum(x) for x in zip(bottom, sorted_executed_lines_counter)]
 
     ax.set_ylim([0, total_nb_runs * 1.1])
     ax.set_xlabel("Line number")
     ax.set_ylabel(f"Number of times the line was executed by a test suite")
 
-    fig.canvas.manager.set_window_title(
-        f"Comparison between {first_experiment_name} and {second_experiment_name}"
-    )
+    experiment_names = " and ".join(all_experiment_name)
+
+    fig.canvas.manager.set_window_title(f"Comparison between {experiment_names}")
 
     plt.xticks(rotation=90, fontsize=6)
     plt.legend()
